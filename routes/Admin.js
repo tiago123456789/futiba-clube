9a666a160f9a24745433b7ed4d5fb77b07510207
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-04-21 13:07:34 -0300

    Added class GrupoDAO

--- routes/Admin.js
+++ routes/Admin.js
@@ -1,8 +1,11 @@
 const gamesRoute = require("./Games");
+const groupRoute = require("./Group");
+const AuthMiddleware = require("./../middleware/AuthMiddleware");
 
 module.exports = (router) => {
     
-    router.use("/games", gamesRoute(router));    
+    router.use("/games", gamesRoute(router));  
+    router.use("/grupos", AuthMiddleware.isPermissionAccess, groupRoute(router));  
 
     return router;
 }
