c92ef14feed0de277abb98108227ce82537d0241
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-05-06 12:29:26 -0300

    Added feature list comments in modal comments

--- routes/Adivinhacao.js
+++ routes/Adivinhacao.js
@@ -6,6 +6,27 @@ const adivinhacaoController  = new AdivinhacaoController(new AdivinhacaoFacade()
 module.exports = (router) => {
 
     router.post("/", adivinhacaoController.save);
+    router.get("/:id/comentarios", (request, response) => {
+        const comentarios = [
+            {
+                "user": {
+                    "name": "Tiago R. da costa"
+                },
+                "comentario": "Ótimo palpite sobre o jogo."
+            },
+            {
+                "user": {
+                    "name": "Tiago R. da costa"
+                },
+                "comentario": "Ótimo palpite sobre o jogo."
+            }
+        ];
+        return response.json(comentarios);
+    });
+
+    // router.post("/:id/comentarios", (request, response) => {
+    //
+    // });
 
     return router;
 };
